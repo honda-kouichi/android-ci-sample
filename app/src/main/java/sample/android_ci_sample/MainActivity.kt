ae27604cb23f7e3f0c3ae24b701cdbe2b54836ec
honda kouichi <honda.kouichi@actindi.net>
2017-02-17 06:25:19 +0900

    バージョン情報を表示

--- app/src/main/java/sample/android_ci_sample/MainActivity.kt
+++ app/src/main/java/sample/android_ci_sample/MainActivity.kt
@@ -2,11 +2,15 @@ package sample.android_ci_sample
 
 import android.os.Bundle
 import android.support.v7.app.AppCompatActivity
+import android.widget.TextView
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        val textView = findViewById(R.id.textView) as TextView
+        textView.text = "versionName:${this.packageManager.getPackageInfo(this.packageName, 0).versionName}" +
+                " versionCode:${this.packageManager.getPackageInfo(this.packageName, 0).versionCode}"
     }
 }
